ba0e675053265339725e8b5de50d8667262bfbc8
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-05-15 18:15:07 +0200

    adding input validation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            string appName = "ConsoleApp - Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Hiruth Marie Stautland";
+            string description = "Console app based on Brad Traversy's youtube tutorial";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("{0}: version: {1} by {2}.", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+
+            Console.WriteLine("Description: {0}", description);
+
+            Console.WriteLine("What's your name?");
+
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + inputName + ". Let us play a game!", inputName);
+
+            // Game
+            int winningNumber = 3;
+            int guess = 0;
+            int guesses = 0;
+
+            Console.WriteLine("Guess a number from 0 to 10");
+
+            while (guess != winningNumber)       
+            {
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You have to type a number.");
+                    Console.ResetColor();
+
+                    continue;
+                }
+                guess = Int32.Parse(input);
+                guesses += 1;
+
+                Console.WriteLine("You guessed: {0}", guess);
+
+                if (guess != winningNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Sorry, {0} is the wrong nomber!", guess);
+                    Console.WriteLine("Please try again!");
+                    Console.ResetColor();
+                } else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Congratulation!");
+                    Console.WriteLine("{0} was the right number!", winningNumber);
+                    Console.WriteLine("You used {0} guesses!", guesses);
+                    Console.ResetColor();
+
+                }
+
+            }
+        }
+    } 
+     
+}
